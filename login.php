8e4a1dd7557ab1eb26f42a11ac0810897d74b37e
abiyyuarianto <abiyyu.arianto99@gmail.com>
2020-02-11 09:24:08 +0700

    coba 1

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php include 'header.php' ?>
+
+<body class="menu-login">
+    <div class="login">
+        <div class="avatar">
+            <i class="fa fa-user"></i>
+        </div>
+        <h2>Login</h2>
+
+        <div class="box-login">
+            <i class="fa fa-envelope"></i>
+            <input type="text" placeholder="Username">
+        </div>
+        <div class="box-login">
+            <i class="fa fa-lock"></i>
+            <input type="text" placeholder="Password">
+        </div>
+
+        <button type="submit" class="btn-login">
+            <a href="dashboard.php" style="color:whitesmoke; text-decoration:none">Login</a>
+
+        </button>
+        <p class="jargon">
+            <b>Start Your Impossible</b>
+        </p>
+        <!-- <div class="button">
+            <a href="">Register</a>
+            <a href=""> Forgot Password</a>
+        </div> -->
+    </div>
+</body>
+
+</html>
